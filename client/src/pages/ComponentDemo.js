9b82a1594e0fe58233b6cead004618d814fa6e01
James Yeates <jimibue@gmail.com>
2021-03-16 10:45:47 -0600

    basic Button added

---
+++ client/src/pages/ComponentDemo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../components/Button'
+
+const ComponentDemo = ()=>{
+    return(
+        <>
+          <h1>My components</h1>
+          <Button primary>Primary button</Button>
+          <Button >Inverted Button</Button>
+        </>
+    )
+}
+
+export default ComponentDemo
