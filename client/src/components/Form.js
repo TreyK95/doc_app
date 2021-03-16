97b1cf33b2c42c1036e57bc743bf6aa6b5e3497d
James Yeates <jimibue@gmail.com>
2021-03-16 13:06:30 -0600

    styled demo

---
+++ client/src/components/Form.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Form = styled.form`
+  display: flex;
+  flex-direction:column;
+  just-content:center;
+`
+
+export default Form
--- client/src/pages/ComponentDemo.js
+++ client/src/pages/ComponentDemo.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Button from '../components/Button'
+import CardContainer from '../components/CardContainer'
+import FormattedMessage from '../components/FormattedMessage'
+import RegisterFormDemo from '../components/RegisterFormDemo'
 
 const ComponentDemo = ()=>{
     return(
@@ -7,7 +10,33 @@ const ComponentDemo = ()=>{
           <h1>My components</h1>
           <Button primary>Primary button</Button>
           <Button >Inverted Button</Button>
-        </>
+          <CardContainer>
+              <FormattedMessage type='alert'>
+                  Alert message
+              </FormattedMessage>
+              <FormattedMessage type='warn'>
+                  Warn message
+              </FormattedMessage>
+              <FormattedMessage>
+                  Notify message
+              </FormattedMessage>
+          </CardContainer>
+          <CardContainer>
+            <RegisterFormDemo />
+         </CardContainer>
+          <CardContainer>
+              <h1>yo</h1>
+              <p>asdhjfka dkfj alksdj flkaj sdlfkj a lk df</p>
+              <p>asdhjfka dkfj alksdj flkaj sdlfkj a lk df</p>
+          </CardContainer>
+          <CardContainer>
+              <h1>yo</h1>
+              <p>asdhjfka dkfj alksdj flkaj sdlfkj a lk df</p>
+              <p>asdhjfka dkfj alksdj flkaj sdlfkj a lk df</p>
+          </CardContainer>
+
+
+         </>
     )
 }
 
