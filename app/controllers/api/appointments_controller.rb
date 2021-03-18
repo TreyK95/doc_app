4cdd8f12a514f33abd7a5a72a74c3e129cc7cf12
zyzzyxis <13ezimm@gmail.com>
2021-03-18 15:04:08 -0600

    appointments controller, app.js routes udpated.

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,2 +1,38 @@
 class Api::AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :destroy]
+  
+  def index
+    appointments = Appointment.all
+    render json: appointments
+  end
+
+  def show
+    render json: @appointment
+  end
+
+  def create
+    appointment = Appointment.new(appointment_params)
+    if(appointment.save)
+      render json: appointment
+    else
+      render json: {error: @appointment.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    render json: @appointment
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:appointment_date, :physician_id, :patient_id)
+  end
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
+
 end
--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import NavBar from './components/NavBar';
 import About from './pages/About';
 import { PRIMARY_COLOR } from './styles';
 import ComponentDemo from './pages/ComponentDemo';
+import Patients from './pages/Patients';
+import PatientShow from './pages/PatientShow';
+import Physicians from './pages/Physicians';
+import PhysicianShow from './pages/PhysicianShow';
+import Appointments from './pages/Appointments';
 
 function App() {
 
@@ -19,7 +24,11 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
         <Route exact path='/componentDemo' component={ComponentDemo} />
-        
+        <Route exact path='/patients' component={Patients} />
+        <Route exact path='/patients/:id' component={PatientShow} />
+        <Route exact path='/physicians' component={Physicians} />
+        <Route exact path='/physicians/:id' component={PhysicianShow} />
+        <Route exact path='/appointments' component={Appointments} />
       </Switch>
       </Container>
    </>
