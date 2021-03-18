762d81488c61dd3338b8f33f80c1364dda3113d0
Trey Kemp <kemptrey@outlook.com>
2021-03-18 15:01:06 -0600

    physician controller done

--- app/controllers/api/physicians_controller.rb
+++ app/controllers/api/physicians_controller.rb
@@ -1,2 +1,36 @@
 class Api::PhysiciansController < ApplicationController
+  before_action :set_physician, only [:show, :destroy]
+
+  def index
+    physicians = Physician.all
+    render json: physicians
+  end 
+
+  def show
+    render json: @physician
+  end
+
+  def create
+    @Physician = Physician.new(physician_params)
+
+    if @physician.save
+      render json: @physician
+    else
+      render json: {error: @physician.errors}, status: 422
+  end
+
+  def destroy
+    @physician.desroy
+    render json: @physician
+  end
+
+  private 
+
+  def set_physician
+    @physician = Physician.find(params[:id])    
+  end
+
+  def physician_params
+    params.require(:physician).permit(:name)
+  end
 end
