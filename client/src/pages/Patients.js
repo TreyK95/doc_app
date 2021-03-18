3faf1303996c14880086abc6e8bb5a5424fcdbeb
jamesElls <jamellsworth@gmail.com>
2021-03-18 16:05:28 -0600

    Patient Show Fixes

--- client/src/pages/Patients.js
+++ client/src/pages/Patients.js
@@ -2,6 +2,9 @@ import React from 'react'
 import AxiosContainer from '../components/AxiosContainer'
 import List from '../components/List'
 import useAxiosOnMount from '../hooks/useAxiosOnMount'
+import {Link} from 'react-router-dom'
+import CardContainer from '../components/CardContainer'
+
 
 const Patients = (props) => {
     const {data, loading, error} = useAxiosOnMount('/api/patients')
@@ -9,15 +12,15 @@ const Patients = (props) => {
         <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading URL, please wait'}>
             <List name='Patients'
                 data={data}
-                // renderData={(user)=> { 
-                //     return(
-                //         <Link to={`/users/${user.id}`}>
-                //           <CardContainer>
-                //               <p>{user.name}</p>
-                //           </CardContainer>
-                //         </Link>
-                //     )} 
-                //  }
+                renderData={(user)=> { 
+                    return(
+                        <Link to={`/users/${user.id}`}>
+                          <CardContainer>
+                              <p>{user.name}</p>
+                          </CardContainer>
+                        </Link>
+                    )} 
+                 }
             />
         </AxiosContainer>
     )
