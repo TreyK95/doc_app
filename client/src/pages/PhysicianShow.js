ca4ff417ab7001dafe1364638df763fc53526f43
Trey Kemp <kemptrey@outlook.com>
2021-03-18 17:11:57 -0600

    added card to single physician

--- client/src/pages/PhysicianShow.js
+++ client/src/pages/PhysicianShow.js
@@ -2,12 +2,15 @@ import useAxiosOnMount from '../hooks/useAxiosOnMount'
 import AxiosContainer from '../components/AxiosContainer'
 import StringifyJson from '../components/StringifyJson'
 import {useParams} from 'react-router-dom'
+import Card from '../components/Card'
 const PhysicianShow = (props)=>{
   const {id} = useParams()
   const {data, loading, error} = useAxiosOnMount(`/api/physicians/${id}`) 
  return (
-     <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading URL, please wait'}>
-        <StringifyJson json={data}/>
+     <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading Physicians, please wait'}>
+        {data &&
+        <Card header={data.name} children={data.id}/>
+        }
      </AxiosContainer>
  )
 }
